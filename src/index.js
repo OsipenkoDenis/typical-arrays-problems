ca40ce0e8d8c08d3d4eba309bda86085f24eda38
osipenkoDenis <denis.osipenko@inbox.ru>
2020-10-01 00:13:17 +0300

    feat: solve a tack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!(Array.isArray(array)) || array.length === 0){
+    return 0;
+  };
+  var minValue = Math.min.apply(null, array);
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!(Array.isArray(array)) || array.length === 0){
+    return 0;
+  };
+  var maxValue = Math.max.apply(null, array);
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!(Array.isArray(array)) || array.length === 0){
+    return 0;
+  };
+  var sum = 0;
+  for(let i = 0; i < array.length;i++){
+    sum+=array[i];
+  };
+  var avgValue = sum/array.length;
+  return avgValue;
 }
